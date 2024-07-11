4e2722e910d7edd8fefbcf76069ae64e9ba123d5
Suryaprasath <suryajack27@gmail.com>
2024-07-11 19:10:53 +0530

    lasst

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+ import Product from '../components/Product'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>This is home page</h1>
+        <Product />
+    </div>
+  )
+}
+
+export default Home
